d6b45ecb6c0eed31eef095a2c27f0bd8666d0176
Jesus Murua <Jesusmuruahh@live.com.mx>
2016-01-06 19:14:13 -0700

    Primer CRUD

---
+++ Demo1/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Demo1.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+        public String Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LastBuy { get; set; }
+        public float Stock { get; set; }
+        public String Remarks { get; set; }
+
+    }
+}
